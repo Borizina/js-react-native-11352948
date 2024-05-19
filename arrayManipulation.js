3a9400d94b49dda68f46e70d44cefdb70856171d
Borizina <eborizina@st.ug.edu.gh>
2024-05-18 22:00:29 -0800

    Task 1

---
+++ arrayManipulation.js
@@ -0,0 +1,9 @@
+function procesArray(arr) {
+    return arr.map(num => {
+        if (num % 2 === 0) {
+            return num * num;
+        } else {
+            return num * 3;
+        }
+    })
+}
